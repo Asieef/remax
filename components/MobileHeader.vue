8e7b4f4cf30a90d4a69190b102389a0b017cdb8b
Asieef <asieef.chowdhury@gmail.com>
2022-05-19 13:51:48 +0600

    mobile menu work

--- components/MobileHeader.vue
+++ components/MobileHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="font-display">
     <div class="grid grid-cols-6 bg-black h-12">
       <div class="col-span-4 flex items-center">
         <NuxtLink to="/">
@@ -44,9 +44,236 @@
         </div>
       </transition>
 
-      <div class="col-span-1 flex items-center justify-center">
-        <img src="/ham.png" class="h-6" @click="ToggleMenu" />
+      <!-- Off Canvas  -->
+      <div class="col-span-1 flex items-center justify-center text-sm">
+        <div class="flex space-x-2">
+          <div>
+            <button
+              class=""
+              type="button"
+              data-bs-toggle="offcanvas"
+              data-bs-target="#offcanvasExample"
+              aria-controls="offcanvasExample"
+            >
+              <img src="/ham.png" class="h-6" />
+            </button>
+
+            <div
+              class="offcanvas offcanvas-start fixed bottom-0 flex flex-col max-w-full bg-white invisible bg-clip-padding shadow-sm outline-none transition duration-300 ease-in-out text-gray-700 top-0 left-0 border-none w-72"
+              tabindex="-1"
+              id="offcanvasExample"
+              aria-labelledby="offcanvasExampleLabel"
+            >
+              <div
+                class="offcanvas-header flex items-center justify-between p-4"
+              >
+                <h5
+                  class="offcanvas-title mb-0 leading-normal font-semibold"
+                  id="offcanvasExampleLabel"
+                >
+                  Product Categories
+                </h5>
+                <button
+                  type="button"
+                  class="btn-close box-content w-4 h-4 p-2 -my-5 -mr-2 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
+                  data-bs-dismiss="offcanvas"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="offcanvas-body flex-grow p-4 overflow-y-auto">
+                <div>
+                  <div class="accordion" id="accordionExample">
+                    <div class="accordion-item bg-white border border-gray-200">
+                      <h2 class="accordion-header mb-0" id="headingOne">
+                        <button
+                          class="accordion-button relative flex items-center w-full py-4 px-5 text-base text-gray-800 text-left bg-white border-0 rounded-none transition focus:outline-none"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#collapseOne"
+                          aria-expanded="true"
+                          aria-controls="collapseOne"
+                        >
+                          Creative Lifestyle
+                        </button>
+                      </h2>
+                      <div
+                        id="collapseOne"
+                        class="accordion-collapse collapse show"
+                        aria-labelledby="headingOne"
+                        data-bs-parent="#accordionExample"
+                      >
+                        <div class="accordion-body py-4 px-5">
+                          <div class="flex justify-center">
+                            <ul class="bg-white rounded-lg w-96 text-gray-900">
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg"
+                              >
+                                Selfie Stick
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                Laptop Bag
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="accordion-item bg-white border border-gray-200">
+                      <h2 class="accordion-header mb-0" id="headingTwo">
+                        <button
+                          class="accordion-button collapsed relative flex items-center w-full py-4 px-5 text-base text-gray-800 text-left bg-white border-0 rounded-none transition focus:outline-none"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#collapseTwo"
+                          aria-expanded="false"
+                          aria-controls="collapseTwo"
+                        >
+                          Audio
+                        </button>
+                      </h2>
+                      <div
+                        id="collapseTwo"
+                        class="accordion-collapse collapse"
+                        aria-labelledby="headingTwo"
+                        data-bs-parent="#accordionExample"
+                      >
+                        <div class="accordion-body py-4 px-5">
+                          <div class="flex justify-center">
+                            <ul class="bg-white rounded-lg w-96 text-gray-900">
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg"
+                              >
+                                Bluetooth Speaker
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                Wired Headphone & Earphone
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                BT Headphone & Earphone
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                Voice Recorder
+                              </li>
+                              <li class="px-6 py-2 w-full rounded-b-lg">
+                                Microphone
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="accordion-item bg-white border border-gray-200">
+                      <h2 class="accordion-header mb-0" id="headingThree">
+                        <button
+                          class="accordion-button collapsed relative flex items-center w-full py-4 px-5 text-base text-gray-800 text-left bg-white border-0 rounded-none transition focus:outline-none"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#collapseThree"
+                          aria-expanded="false"
+                          aria-controls="collapseThree"
+                        >
+                          Protection
+                        </button>
+                      </h2>
+                      <div
+                        id="collapseThree"
+                        class="accordion-collapse collapse"
+                        aria-labelledby="headingThree"
+                        data-bs-parent="#accordionExample"
+                      >
+                        <div class="accordion-body py-4 px-5">
+                          <div class="flex justify-center">
+                            <ul class="bg-white rounded-lg w-96 text-gray-900">
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg"
+                              >
+                                Mobile Case
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="accordion-item bg-white border border-gray-200">
+                      <h2 class="accordion-header mb-0" id="headingFour">
+                        <button
+                          class="accordion-button collapsed relative flex items-center w-full py-4 px-5 text-base text-gray-800 text-left bg-white border-0 rounded-none transition focus:outline-none"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#collapseFour"
+                          aria-expanded="false"
+                          aria-controls="collapseFour"
+                        >
+                          Energy
+                        </button>
+                      </h2>
+                      <div
+                        id="collapseFour"
+                        class="accordion-collapse collapse"
+                        aria-labelledby="headingFour"
+                        data-bs-parent="#accordionExample"
+                      >
+                        <div class="accordion-body py-4 px-5">
+                          <div class="flex justify-center">
+                            <ul class="bg-white rounded-lg w-96 text-gray-900">
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full rounded-t-lg"
+                              >
+                                USB Hub
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                Wireless Charger
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                Power Strip
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                USB/PD Charger
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                Car Charger
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                Power Bank
+                              </li>
+                              <li
+                                class="px-6 py-2 border-b border-gray-200 w-full"
+                              >
+                                Data & Charging Cable
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+      <!-- Ends Off Canvas -->
     </div>
   </div>
 </template>
